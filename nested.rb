3598bc5bac636f8fdf4671ee3e639a64956d3f3e
Adam Palmer <theadampalmer@gmail.com>
2020-07-15 16:07:14 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -2,7 +2,7 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {},
+        grace_hopper: {},
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },

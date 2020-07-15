30cca8101f99ba21704c861ae0f220d308f27483
Adam Palmer <theadampalmer@gmail.com>
2020-07-15 16:21:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,25 @@ programmer_hash[:grace_hopper]
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+     programmer_hash[:alan_kay][:known_for]
+end
+
 	programmer_hash = 
  		{
         :grace_hopper => {

83c41aa4160478062f7fb6f874977ad5b1920f06
Adam Palmer <theadampalmer@gmail.com>
2020-07-15 16:09:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,13 +6,13 @@ def hopper
           known_for: "COBOL",
           languages: ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {},
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+        alan_kay: {},
+          known_for: "Object Orientation",
+          languages: ["Smalltalk", "LISP"]
         },
-        :dennis_ritchie => {}
-          :known_for => "Unix",
-          :languages => ["C"]
+        dennis_ritchie: {}
+          known_for: "Unix",
+          languages: ["C"]
         }
      }
 
